7027e5a04437fc23ec4eb0b23d7ab06b725a075d
Jared Nolen <Legodoghead@gmail.com>
2021-10-13 15:28:25 -0700

    Have a working Grid + placement system

--- crisp-game-lib/docs/cityplan/main.js
+++ crisp-game-lib/docs/cityplan/main.js
@@ -3,40 +3,122 @@ title = "cityplan";
 description = `
 `;
 
-characters = [];
+characters = [
+`
+bbbbb
+b   b
+b   b
+b   b
+bbbbb
+`,
+];
 
-let width = 100;
-let height = 100;
+const S = {
+	WIDTH: 100,     // Game Width
+  HEIGHT: 100,    // Game Height
+  GRIDSIZE: 10,   // Size of quares in the grid
+  GRIDNUM: 8,     // Number of rows / columns in grid
+  BOUNDX: 0,      // Grid buffer on x
+  BOUNDY: 0       // Grid buffer on y
+};
 
 options = {
   viewSize: {
-  x: width,
-  y: height,
+  x: S.WIDTH,
+  y: S.HEIGHT,
   }
 };
 
+// Declaring stuff
+let dict = new Map();
+let cursor;
+
 function update() {
   if (!ticks) {
-    // drawGrid(10,1);
+    // Cursor
+    cursor = vec(S.WIDTH * 0.5, S.HEIGHT * 0.5)
+
+    // Setup Grid Dictionary
+    for(let i = 1; i <= S.GRIDNUM; i++){
+      for(let j = 1; j <= S.GRIDNUM; j++){
+        const xpos = S.BOUNDX+(i*(S.GRIDSIZE));
+        const ypos = S.BOUNDY+(j*(S.GRIDSIZE));
+        const tempA = [xpos, ypos];
+        const tempS = tempA.join(',');
+        dict.set(tempS, "empty");
+      }
+    }
   }
-  drawGrid(8,8,8);
-  // line(8,0,8,height,1);
-  // line(0,8,width,8,1);
+  
+  //Create Grid
+  Filldict(S.GRIDNUM, S.GRIDSIZE, S.BOUNDX, S.BOUNDY);
+
+  // Cursor
+  color("light_purple");
+  let mouse = rect(cursor, 1, 1);
+  cursor = vec(input.pos.x - (input.pos.x % S.GRIDSIZE), input.pos.y - (input.pos.y % S.GRIDSIZE));
+  
+  // Cursor collison check
+  colCheck(mouse)
+  
 }
 
-function drawGrid(gSize,startx,starty){
+/*function drawGrid(gSize,startx,starty){
   color("red");
-  for(let x = 0; x <= width; x += gSize){
+  for(let x = 0; x <= S.WIDTH; x += gSize){
     // console.log("x=" + x);
-    line(x,starty,x,height,1);
-    line(x,starty+1,x,height+1,1);
-    line(x,starty+2,x,height+2,1);
+    line(x,starty,x,S.HEIGHT,1);
+    line(x,starty+1,x,S.HEIGHT+1,1);
+    line(x,starty+2,x,S.HEIGHT+2,1);
   }
-  for(let y = 0; y <= height; y += gSize){
+  for(let y = 0; y <= S.HEIGHT; y += gSize){
     // console.log("y="+ y);
     // console.log("x=" + x + " y="+ y);
-    line(startx,y,width,y,1);
-    line(startx+1,y,width+1,y,1);
-    line(startx+2,y,width+2,y,1);
+    line(startx,y,S.WIDTH,y,1);
+    line(startx+1,y,S.WIDTH+1,y,1);
+    line(startx+2,y,S.WIDTH+2,y,1);
+  }
+}*/
+
+function Filldict(num, size, boundx, boundy){
+  for(let i = 1; i <= num; i++){
+    for(let j = 1; j <= num; j++){
+      const xpos = boundx+(i*(size));
+      const ypos = boundy+(j*(size));
+      const tempA = [xpos, ypos];
+      const tempS = tempA.join(',');
+      // Draw Land
+      color("green");
+      rect(xpos, ypos, size, size);
+      // Draw Buildings
+      if(dict.get(tempS)=="house"){
+        color("black");
+        char('a', xpos+(S.GRIDSIZE/2), ypos+(S.GRIDSIZE/2));
+      }
+    }
+  }
+  //console.log(dict)
+}
+
+function colCheck(mouse){
+  if(mouse.isColliding.rect.green){
+    const xpos = input.pos.x - (input.pos.x % S.GRIDSIZE);
+    const ypos = input.pos.y - (input.pos.y % S.GRIDSIZE);
+    const tempA = [xpos, ypos];
+    const tempS = tempA.join(',');
+
+    // Highlight selected area with box
+    color("yellow");
+    rect(xpos, ypos, 1, S.GRIDSIZE);
+    rect(xpos, ypos, S.GRIDSIZE, 1);
+    rect(xpos+S.GRIDSIZE, ypos, 1, S.GRIDSIZE);
+    rect(xpos, ypos+S.GRIDSIZE, S.GRIDSIZE+1, 1);
+
+    // Place building
+    if(input.isJustPressed){
+      dict.set(tempS, "house");
+      color("black");
+      char('a', xpos+3, ypos+3);
+    }
   }
 }
